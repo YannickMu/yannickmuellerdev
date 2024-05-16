0b24383090fa5e6939f0f6dbbf1d6c4b72e7b5b5
Yannick Mueller <info@yannickmueller.dev>
2024-05-16 17:11:34 +0200

    Nothing really changed

--- FastAPI/src/rye_yannickmueller_dev/db.py
+++ FastAPI/src/rye_yannickmueller_dev/db.py
@@ -13,67 +13,67 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 
 
 class Signup:
-    """
-    In this class are the functions for the Signup process.
-    """
-    async def signup(self, username, fname, lname, email, phone, password, chpassword, salt, captcha):
-        if salt == captcha:
-            if phone == "":
-                phone = 'NULL'
-            try:
-                conn = mariadb.connect(
-                    host="172.15.15.3", port=3306, user="README", password="README"
-                )
-                cur = conn.cursor()
-                cur.execute("USE App;")
-                if phone == "NULL":
-                    cur.execute(
-                        f"""INSERT INTO Login (username, fname, lname, email, phone, password, salt)
-                        VALUES ('{username}', '{fname}', '{lname}', '{email}', '{phone}','{password}', '{salt}')"""
-                    )
-                else:
-                    cur.execute(
-                        f"""INSERT INTO Login (username, fname, lname, email, phone, password, salt)
-                        VALUES ('{username}', '{fname}', '{lname}', '{email}', '{phone}','{password}', '{salt}')"""
-                    )
-                conn.commit()
-                conn.close()
-                return 'true'
-            except Exception as e:
-                return f"Error: {e}"
-        else:
-            return "false"
+	"""
+	In this class are the functions for the Signup process.
+	"""
+	async def signup(self, username, fname, lname, email, phone, password, chpassword, salt, captcha):
+		if salt == captcha:
+			if phone == "":
+				phone = 'NULL'
+			try:
+				conn = mariadb.connect(
+					host="172.15.15.3", port=3306, user="README", password="README"
+				)
+				cur = conn.cursor()
+				cur.execute("USE App;")
+				if phone == "NULL":
+					cur.execute(
+						f"""INSERT INTO Login (username, fname, lname, email, phone, password, salt)
+						VALUES ('{username}', '{fname}', '{lname}', '{email}', '{phone}','{password}', '{salt}')"""
+					)
+				else:
+					cur.execute(
+						f"""INSERT INTO Login (username, fname, lname, email, phone, password, salt)
+						VALUES ('{username}', '{fname}', '{lname}', '{email}', '{phone}','{password}', '{salt}')"""
+					)
+				conn.commit()
+				conn.close()
+				return 'true'
+			except Exception as e:
+				return f"Error: {e}"
+		else:
+			return "false"
 
 
 class Login:
-    async def get(self, username, password):
-        try:
-            conn = mariadb.connect(
-                host="172.15.15.3", port=3306, user="README", password="README"
-            )
-            cur = conn.cursor()
-            cur.execute("USE App;")
-            cur.execute(
-                f"SELECT username FROM Login WHERE password = '{password}' AND username = '{username}'"
-            )
-            out = cur.fetchall()
-            conn.commit()
-            conn.close()
-            return out
-        except Exception as e:
-            return f"Error: {e}"
+	async def get(self, username, password):
+		try:
+			conn = mariadb.connect(
+				host="172.15.15.3", port=3306, user="README", password="README"
+			)
+			cur = conn.cursor()
+			cur.execute("USE App;")
+			cur.execute(
+				f"SELECT username FROM Login WHERE password = '{password}' AND username = '{username}'"
+			)
+			out = cur.fetchall()
+			conn.commit()
+			conn.close()
+			return out
+		except Exception as e:
+			return f"Error: {e}"
 class GetSalt:
-    async def gets(self, username):
-        try:
-            conn = mariadb.connect(
-                host="172.15.15.3", port=3306, user="README", password="README"
-            )
-            cur = conn.cursor()
-            cur.execute("USE App;")
-            cur.execute(f"SELECT salt FROM Login WHERE username = '{username}'")
-            salt = cur.fetchall()[0][0]
-            conn.commit()
-            conn.close()
-        except Exception as e:
-            return f"Error: {e}"
-        return salt
+	async def gets(self, username):
+		try:
+			conn = mariadb.connect(
+				host="172.15.15.3", port=3306, user="README", password="README"
+			)
+			cur = conn.cursor()
+			cur.execute("USE App;")
+			cur.execute(f"SELECT salt FROM Login WHERE username = '{username}'")
+			salt = cur.fetchall()[0][0]
+			conn.commit()
+			conn.close()
+		except Exception as e:
+			return f"Error: {e}"
+		return salt
